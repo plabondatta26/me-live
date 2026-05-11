06a378681496240ab3c45081e661f51ecf351f33
Plabon Datta <plabondatta26@gmail.com>
2026-05-12 02:29:09 +0600

    livekit URL changed

--- me_live/utils/constants.py
+++ me_live/utils/constants.py
@@ -4,4 +4,4 @@ kRoomPrefix = 'melive_dev'
 # Prod
 # liveRoomSocketBaseUrl = 'ws://5.252.54.94/ws/livekit-live-room'
 # Dev
-liveRoomSocketBaseUrl = 'ws://0.0.0.0:8000/ws/livekit-live-room'
+liveRoomSocketBaseUrl = 'ws://144.91.123.53:8000/ws/livekit-live-room'
